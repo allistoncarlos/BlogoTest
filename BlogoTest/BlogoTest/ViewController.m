732cece590aaa14431c1954dd55b0d11fdec756d
Alliston Aleixo <alliston@outlook.com>
2015-10-26 19:31:18 -0200

    Prévia da tela inicial (foco no iPad por enquanto)

--- BlogoTest/BlogoTest/ViewController.m
+++ BlogoTest/BlogoTest/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *recentSearchesLabel;
 
 @end
 
@@ -22,6 +23,13 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self performSegueWithIdentifier:@"ShowResults" sender:self];
+    [textField resignFirstResponder];
+    
+    return YES;
 }
 
 @end
